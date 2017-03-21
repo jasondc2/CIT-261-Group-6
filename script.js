3e19ee49ee74d05d3b302bc9c33869f5e2e09219
dkbrewer <dantheman1990@comcast.net>
2017-03-21 17:24:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function getBig(x) {
     x.style.height = "200px";
     x.style.width = "200px";
+    x.style.border = "thick solid #0000FF"
     x.style.WebkitTransitionDuration = "1s"; 
     x.style.transitionDuration = "1s"; 
+    
 }
 
 function getNorm(x) {
