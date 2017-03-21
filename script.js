592b260f4540c25b2a94de6bd08eae1d5c2e0f2f
dkbrewer <dantheman1990@comcast.net>
2017-03-21 17:31:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+//These functions increase and decrease the image sizes based on mouse over and mouse out
+
 function getBig(x) {
     x.style.height = "200px";
     x.style.width = "200px";
     x.style.border = "thick solid #0000FF"
     x.style.WebkitTransitionDuration = "1s"; 
     x.style.transitionDuration = "1s"; 
-    
 }
 
 function getNorm(x) {
     x.style.height = "150px";
     x.style.width = "150px";
+    x.style.border = "none"
 }
