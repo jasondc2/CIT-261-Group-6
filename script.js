4328cab98415696d06d1000b25e18647a02116da
dkbrewer <dantheman1990@comcast.net>
2017-03-21 17:12:39 -0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function getBig(x) {
+    x.style.height = "200px";
+    x.style.width = "200px";
+    x.style.WebkitTransitionDuration = "1s"; 
+	  x.style.transitionDuration = "1s"; 
+}
+
+function getNorm(x) {
+    x.style.height = "150px";
+    x.style.width = "150px";
+}
