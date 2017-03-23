9518e64b341b6c14f4396e242a24f209a5344a38
MillerLoren <lorenmillerp@gmail.com>
2017-03-23 13:06:59 -0600

    Paging is now working.
    Menu buttons will change the pages. I updated the gallery items to be
    more dynamic so we can use it elsewhere.

--- script.js
+++ script.js
@@ -1,69 +1,35 @@
 //These functions increase and decrease the image sizes based on mouse over and mouse out
-var holder_0 = document.getElementById("inside_shrink");
-var holder_1 = document.getElementById("inside_shrink_2");
+var holder = document.getElementsByClassName("inside_shrink");
+var galleryItems = new Array(holder.length);
 var which = 0;
-holder_0.style.left = "";
-holder_0.style.display = "";
-holder_0.style["transition"] = "all 2s linear";
-holder_1.style["transition"] = "all 2s linear";
-var galleryItems_0 = holder_0.getElementsByClassName("gallery_item");
-var galleryItems_1 = holder_1.getElementsByClassName("gallery_item");
+for(var i = 0; i < holder.length; i++){
+	galleryItems[i] = holder[i].getElementsByClassName("gallery_item");
+}
 var galItemPosX = 0;
 var galItemPosY = 0;
-for(var i = galleryItems_0.length - 1; i != -1; i--){
-    galleryItems_0[i].style.height = "150px";
-    galleryItems_0[i].style.width = "150px";
-    galleryItems_0[i].style["boxShadow"] = "";
-	console.log((i + 1) % 5 == 0);
-	if((i + 1) % 5 == 0 && i+1 != 10){
-		galItemPosY += 161;
-		galItemPosX = 0;
-	}
-	galleryItems_0[i].style.left = galItemPosX + "px";
-	galleryItems_0[i].style.top = galItemPosY + "px";
-	galItemPosX += 161;
-    galleryItems_0[i].style.WebkitTransitionDuration = "1s"; 
-    galleryItems_0[i].style.transitionDuration = "1s";
-	galleryItems_0[i].style.zIndex = 2;
-}
-galItemPosX = 0;
-galItemPosY = 0;
-for(var i = galleryItems_1.length - 1; i != -1; i--){
-    galleryItems_1[i].style.height = "150px";
-    galleryItems_1[i].style.width = "150px";
-    galleryItems_1[i].style["boxShadow"] = "";
-	console.log((i + 1) % 5 == 0);
-	if((i + 1) % 5 == 0 && i+1 != 10){
-		galItemPosY += 161;
-		galItemPosX = 0;
-	}
-	galleryItems_1[i].style.left = galItemPosX + "px";
-	galleryItems_1[i].style.top = galItemPosY + "px";
-	galItemPosX += 161;
-    galleryItems_1[i].style.WebkitTransitionDuration = "1s"; 
-    galleryItems_1[i].style.transitionDuration = "1s";
-	galleryItems_1[i].style.zIndex = 2;
-}
+var myIndex = 0;
+start();
+carousel();
 function moveGallery(n){
 	if(n == 0){
 		if(which == 1){
-			holder_1.style.left = "-150%";
-			holder_0.style.left = "0px";
+			holder[1].style.left = "-150%";
+			holder[0].style.left = "0px";
 			which = 0;
 		}else{
-			holder_0.style.left = "150%";
-			holder_1.style.left = "0px";
+			holder[0].style.left = "150%";
+			holder[1].style.left = "0px";
 			which = 1;
 		}
 	}
 	else if(n == 1){
 		if(which == 1){
-			holder_1.style.left = "-150%";
-			holder_0.style.left = "0px";
+			holder[1].style.left = "-150%";
+			holder[0].style.left = "0px";
 			which = 0;
 		}else{
-			holder_0.style.left = "150%";
-			holder_1.style.left = "0px";
+			holder[0].style.left = "150%";
+			holder[1].style.left = "0px";
 			which = 1;
 		}
 	}
@@ -86,10 +52,44 @@ function getNorm(x) {
 	x.style.zIndex = 2;
 }
 
-
-var myIndex = 0;
-carousel();
-
+function start(){
+	holder[0].style.left = "";
+	holder[0].style.display = "";
+	holder[0].style["transition"] = "all 2s linear";
+	holder[1].style["transition"] = "all 2s linear";
+	for(var j = 0; j < holder.length; j++){
+		var count = 1;
+		for(var i = galleryItems[j].length - 1; i != -1; i--){
+			galleryItems[j][i].style.height = "150px";
+			galleryItems[j][i].style.width = "150px";
+			galleryItems[j][i].style["boxShadow"] = "";
+			if((i + 1) % 5 == 0 && (i + 1) != galleryItems[j].length){
+				galItemPosY += 160;
+				galItemPosX = 0;
+				count++;
+			}
+			galleryItems[j][i].style.left = galItemPosX + "px";
+			galleryItems[j][i].style.top = galItemPosY + "px";
+			galItemPosX += 160;
+			galleryItems[j][i].style.WebkitTransitionDuration = "1s"; 
+			galleryItems[j][i].style.transitionDuration = "1s";
+			galleryItems[j][i].style.zIndex = 2;
+		}
+		var amount = count * 160;
+		holder[j].parentElement.parentElement.style.height = amount + "px";
+		galItemPosX = 0;
+		galItemPosY = 0;
+	}
+	
+	var content = document.getElementsByClassName("content");
+	for(var i = 0; i < content.length; i++){
+		if(i === 0){
+			hideHelper(i,0);
+		}else{
+			content[i].style.display = "none";
+		}
+	}
+}
 function carousel() {
     var i;
     var x = document.getElementsByClassName("slides");
@@ -100,4 +100,43 @@ function carousel() {
     if (myIndex > x.length) {myIndex = 1}    
     x[myIndex-1].style.display = "block";  
     setTimeout(carousel, 5000);
+}
+function pages(n){
+	var content = document.getElementsByClassName("content");
+	for(var i = 0; i < content.length; i++){
+		if(i === n){
+			hideHelper(i,0);
+		}else{
+			hideHelper(i,1);
+		}
+	}
+}
+function hideHelper(i,n){
+	var content = document.getElementsByClassName("content");
+	document.body.style.overflowX = "hidden";
+	if(n === 1){
+		var op = 1;
+		var timer = setInterval(function () {
+			if (op <= 0.1){
+				clearInterval(timer);
+				content[i].style.display = 'none';
+			}
+			content[i].style.opacity = op;
+			content[i].style.filter = 'alpha(opacity=' + op * 100 + ")";
+			op -= op * 0.1;
+		}, 15);
+	}
+	else{
+		var op = 0.1;
+		content[i].style.display = 'block';
+		var timer = setInterval(function () {
+			if (op >= 1){
+				clearInterval(timer);
+			}
+			content[i].style.opacity = op;
+			content[i].style.filter = 'alpha(opacity=' + op * 100 + ")";
+			op += op * 0.1;
+		}, 15);
+	}
+	document.body.style.overflowX = "";
 }
