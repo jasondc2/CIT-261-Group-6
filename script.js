3ab8235c9f882f111f34b69a52e0b90a43d298ed
jasondc2 <jasondchristensen@outlook.com>
2017-04-01 09:15:48 -0600

    Adding elements to grid

--- script.js
+++ script.js
@@ -25,6 +25,11 @@ function Lamp(id, name, price, type, image, description){
     this.description = description;
 }
 function populate(where, item){
+	var div = document.getElementsByClassName("inside_shrink");
+	var div1 = document.createElement("div");
+	div1 = where;
+	//document.createElement("")
+	div.appendChild(div1);
     /*item.getElementById("lamp_name").innerHTML = this.name;
     item.getElementById("lamp_price").innerHTML = this.price;
     item.getElementById("lamp_image").innerHTML = this.image;
